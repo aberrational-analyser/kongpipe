3092455ba5f9cf671475a1e6b213a88f2addf316
Erik <edasher@webmd.net>
2019-03-15 16:11:39 -0700

    Add faketravis.sh and config vagrant to launch it.

--- Vagrantfile
+++ Vagrantfile
@@ -63,8 +63,10 @@ Vagrant.configure("2") do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  # config.vm.provision "shell", inline: <<-SHELL
-  #   apt-get update
-  #   apt-get install -y apache2
-  # SHELL
+  config.vm.provision "shell", inline: <<-SHELL
+    yum install git -y
+    git clone https://github.com/aberrational-analyser/kongpipe
+    cd kongpipe
+    faketravis.sh
+  SHELL
 end
---
+++ faketravis.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# fake travis
+#
+# setup the prerequisites that are normally handled by travis.ci so that we
+# can run tests locally with a debugger...
+#
+
+#---------
+# pull down the repo and unzip.
+#
+sudo yum install git -y
+git clone https://github.com/Kong/kong.git
+cd kong
+
+#----------------------
+# install docker to support Cassandra:
+#----------------------
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager y\
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+
+#install gcc
+sudo yum install -y gcc
+sudo yum install -y patch
+
+# install nginx with PCRE library I think....
+
+
+
+#---------
+# Setup the environment vars normally provided by kong's travis.yml
+# We are selecting the cell from the test matrix that runs against cassandra 
+# 3.9...
+#--------
+export LUAROCKS=3.0.4
+export OPENSSL=1.1.1b
+export OPENRESTY_BASE=1.13.6.2
+export OPENRESTY_LATEST=1.13.6.2
+export OPENRESTY=$OPENRESTY_BASE
+export DOWNLOAD_CACHE=~/kong-download-cache
+export INSTALL_CACHE=~/kong-install-cache
+# export KONG_TEST_PG_DATABASE=travis
+# export KONG_TEST_PG_USER=postgres
+export KONG_TEST_DATABASE=cassandra
+export CASSANDRA=3.9
+export TEST_SUITE=integration
+#export TEST_SUITE=plugins
+#export TEST_SUITE=pdk
+
+#install make dev :(
+
+# source .ci/setup_env.sh
+
+# luacheck -q .
+# bin/busted -v -o gtest spec/01-unit
+
